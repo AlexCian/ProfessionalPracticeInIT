95f12be84b9817605ed48a7b7a6e04630130bbd4
Alex Cherry <moecherry2@gmail.com>
2018-12-02 15:01:05 +0000

    Add files via upload

---
+++ src/app/post-info/post-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-info',
+  templateUrl: './post-info.component.html',
+  styleUrls: ['./post-info.component.css']
+})
+export class PostInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
