8634fc27c542fab64c18b543de61baa713f0b590
Alex Cherry <G00347106@gmit.ie>
2018-12-02 15:00:13 +0000

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,20 +1,18 @@
-var express = require('express');//npm install express
+var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');//npm install mongoose
-
+var mongoose = require('mongoose');
 var mongoDB = 'mongodb://moecherry1:moecherry1@ds255970.mlab.com:55970/lab7';
+
 mongoose.connect(mongoDB);
 
 var Schema = mongoose.Schema;
-
-var postSchema = new Schema ({
-    title : String,
-    content : String
+var postSchema = new Schema({
+    title: String,
+    content: String
 })
-
-var postModel = mongoose.model('post', postSchema);
+var PostModel = mongoose.model('post', postSchema);
 
 //Here we are configuring express to use body-parser as middle-ware. 
 app.use(bodyParser.urlencoded({ extended: false })); 
@@ -22,6 +20,7 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -37,51 +36,40 @@ app.get('/', function (req, res) {
    res.send('Hello from Express');
 })
 
-app.get('/api/posts', function(req, res)
-{
-    postModel.find(function(err, data)
-    {
-        if(err)
-        res.send(err);
-        res.json(data);
-    })
-})
-
 app.post('/api/posts', function(req, res){
-    console.log("post successful");
-    console.log(req.body.title);
-    console.log(req.body.content);
-    postModel.find()
-    res.json();
-    postModel.create({
-    title:req.body.title,
-    content:req.body.content
-    })
+    console.log("Success!");
+    console.log("Topping 1: "+req.body.title);
+    console.log("Topping 2: "+req.body.content);
+
+    PostModel.create({
+        title: req.body.title,
+        content: req.body.content
+    });
+    res.send('Item added');
+
 })
 
 app.get('/api/posts', function(req, res){
-
-    const posts = 
-    [
-        { 
-            "id": "fadf12421l", 
-            "title": "First server-side post", 
-            "content": "This is coming from the server" 
-        }, 
-        { 
-            "id": "ksajflaj132", 
-            "title": "Second server-side post", 
-            "content": "This is coming from the server!" 
-        }
-    ];
-
-    res.status(200).json({posts:posts})
+    PostModel.find(function(err, data){
+        res.json(data);
+    });
 })
 
+app.delete('/api/posts/:id', function(req, res){
+    console.log("Doughnut id deleted : "+req.params.id);
+
+    PostModel.deleteOne({_id:req.params.id},
+    function(err, data)
+    {
+        if(err)
+            res.send(err);
+        res.send(data);
+    })
+})
 
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Host at http://%s:%s", host, port)
 })
