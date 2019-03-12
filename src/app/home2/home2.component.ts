932304b45685b68329fd1e8237f3305d98e391b3
Alex Cherry <moecherry2@gmail.com>
2019-03-12 19:46:15 +0000

    Add files via upload

---
+++ src/app/home2/home2.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {PostService} from '../services/post.service';
+import { Observable } from 'rxjs';
+import {Post} from '../post.model';
+
+@Component({
+  selector: 'app-home2',
+  templateUrl: './home2.component.html',
+  styleUrls: ['./home2.component.css']
+})
+export class Home2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
