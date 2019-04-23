bfb18263ec58c6e95527ab9f6890ad7487fa39e5
Alex Cherry <moecherry2@gmail.com>
2019-04-23 19:55:33 +0100

    Update post-info.component.ts

--- src/app/post-info/post-info.component.ts
+++ src/app/post-info/post-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import {PostService} from '../services/post.service';
 
 @Component({
   selector: 'app-post-info',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:PostService) { }
+
+  onAddPost(form: NgForm) {
+
+    this.service.addPost(form.value.title, form.value.content).subscribe();
+    
+    console.log(form.value);
+    form.resetForm();
+  }
+
 
   ngOnInit() {
+
+
+
   }
 
 }
+ 
